972efba208bef55b81a303496ada324235e7d6c9
Max Davidson <max.davidson@cygni.se>
2018-11-08 17:21:59 +0100

    Deploy to S3 bucket with Terraform and AWS CLI

---
+++ infrastructure/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  version = "~> 1.43"
+  region  = "${var.aws_region}"
+}
+
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket         = "paintbot-terraform-remote-state-storage"
+    dynamodb_table = "paintbot-terraform-state-lock"
+    region         = "eu-west-1"
+    key            = "paintbot-webapp.tfstate"
+  }
+}
+
+resource "aws_s3_bucket" "app_bucket" {
+  bucket_prefix = "${var.bucket_name}-"
+  region        = "${var.aws_region}"
+  acl           = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "read_policy" {
+  bucket = "${aws_s3_bucket.app_bucket.id}"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": ["s3:GetObject"],
+            "Resource": ["${aws_s3_bucket.app_bucket.arn}/*"]
+        }
+    ]
+}
+POLICY
+}
