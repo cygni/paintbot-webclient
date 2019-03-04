0bc5eef3787d9f7352d6abc130e1ddac807f7ccc
Johan Horvius <drhorv@gmail.com>
2019-03-04 20:45:52 +0100

    Add type for GameScreen

--- src/game/GameScreen.tsx
+++ src/game/GameScreen.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { RouteComponentProps } from 'react-router';
 import styled from 'styled-components';
 
 import background from '../resources/background.jpg';
@@ -18,7 +19,7 @@ const WindowContainer = styled.div`
   background-repeat: no-repeat;
 `;
 
-export default function GameScreen(props: any) {
+export default function GameScreen(props: RouteComponentProps<{ id: string }>) {
   return (
     <WindowContainer>
       <GameDirector id={props.match.params.id} />
