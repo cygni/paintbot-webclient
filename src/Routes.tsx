08d72201f64248df35c00f15b2662654be240b2d
Max Davidson <max.davidson@cygni.se>
2019-10-25 11:01:21 +0200

    Switch to NPM and fix audit warnings

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -11,7 +11,7 @@ const Router: ComponentType<ComponentProps<typeof HashRouter> | ComponentProps<t
 // React-Router needs te to figure out were the page is served.
 // PUBLIC_URL gives us the absolute URL where the page is served, so the basename
 // can be computed by resolving this URL relative to the current origin and extracting the pathname.
-const { pathname: BASENAME } = new URL(process.env.PUBLIC_URL, location.origin);
+const { pathname: BASENAME } = new URL(process.env.PUBLIC_URL, window.location.origin);
 
 // The Route's propTypes are incorrect in react-router 4.3, as it assumes all components are functions.
 // This isn't true for new "exotic" components like React.lazy or React.forwardRef, since they return objects.
