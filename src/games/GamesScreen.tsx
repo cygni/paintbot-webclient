060a4fa6b9b2e609ff4d74aeb9244f1b199da2bc
hfurhoff <hfurhoff@kth.se>
2019-11-14 16:51:31 +0100

    minor change in gamesScreen

--- src/games/GamesScreen.tsx
+++ src/games/GamesScreen.tsx
@@ -1,21 +1,5 @@
-import React, { useContext } from 'react';
-
-import { TournamentContext } from '../common/Contexts';
-import Config from '../Config';
+import React from 'react';
 
 export default function GamesScreen(props: any) {
-  const tourContext = useContext(TournamentContext);
-  const getActiveTournament = async () => {
-    const response = await fetch(`${Config.BackendUrl}/tournament/active`);
-    response.text().then(text => {
-      if (response.ok) {
-        console.log(JSON.parse(text));
-      }
-    });
-  };
-  if (tourContext.tournamentId !== '') {
-    getActiveTournament();
-  }
-
-  return <p>Active games</p>;
+  return <h1>Active games</h1>;
 }
