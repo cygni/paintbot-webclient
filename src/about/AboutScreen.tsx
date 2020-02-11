7c72dc0b971096176a9e05c36cee894ef95e7a00
Josefina Andreasson <josefina.andreasson@cygni.se>
2020-02-11 14:43:51 +0100

    Added rule about points for stuns

--- src/about/AboutScreen.tsx
+++ src/about/AboutScreen.tsx
@@ -31,14 +31,15 @@ export default function AboutScreen() {
           <ul>
             <li>Each client must respond within 250ms</li>
             <li>1 point per tile owned</li>
-            <li>1 power-up at a time may be carried</li>
+            <li>5 points per stun caused (through power-ups)</li>
+            <li>10 ticks before stun is over</li>
+            <li>3 ticks of invulnerability after being stunned</li>
+            <li>5 holes</li>
+            <li>Bots may carry one power-up at a time</li>
             <li>The power-up may be used at any point</li>
             <li>Using the power-up fills the tiles within Manhattan distance 4 from your bot</li>
             <li>Any other bots within the area where a power-up is used will be stunned</li>
             <li>Moving into a hole, the edge of the map or another bot will stun you</li>
-            <li>10 ticks before stun is over</li>
-            <li>3 ticks of invulnerability after being stunned</li>
-            <li>5 holes</li>
             <li>The bot with the most tiles in the end wins</li>
           </ul>
         </PaperRow>
