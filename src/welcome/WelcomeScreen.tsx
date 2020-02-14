b201526724bae7f920ad19d4107c4a7c4bc4a831
Josefina Andreasson <josefina.andreasson@cygni.se>
2020-02-14 09:45:58 +0100

    Fixed some warnings

--- src/welcome/WelcomeScreen.tsx
+++ src/welcome/WelcomeScreen.tsx
@@ -31,6 +31,7 @@ export default function WelcomeScreen(props: any) {
             width="560"
             height="315"
             src="https://www.youtube.com/embed/G6M7RpQaInQ"
+            title="Paintbot gameplay"
             frameBorder={0}
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
